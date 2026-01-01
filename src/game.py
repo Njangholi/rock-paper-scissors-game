cc3a9ca5cdca408a59f56bd3f0c0068d11b5870c
Narges Jangholi <jangholi83@gmail.com>
2026-01-01 21:44:33 +0330

    static code analysis-black

--- src/game.py
+++ src/game.py
@@ -1,7 +1,7 @@
 """Play a round of Rock, Paper, Scissors.
- Author: Narges Jangholi
- Date: 2025-09-18
- Description: This module contains the main class and methods for game of Rock, Paper, Scissors.
+Author: Narges Jangholi
+Date: 2025-09-18
+Description: This module contains the main class and methods for game of Rock, Paper, Scissors.
 """
 
 import random
@@ -10,36 +10,31 @@ from typing import List
 
 class RockPaperScissors:
     """Main class to represent the game of Rock, Paper, Scissors.
-    
-    This class handles player input, computer choice, 
+
+    This class handles player input, computer choice,
     and determines the winner of each round.
     """
 
     # Win combinations where the first item beats the second item.
-    WIN_COMBINATIONS = [
-        ('rock', 'scissors'),
-        ('scissors', 'paper'),
-        ('paper', 'rock')
-    ]
+    WIN_COMBINATIONS = [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]
 
     def __init__(self, name: str):
-        self.choices: List[str] = ['rock', 'paper', 'scissors']
+        self.choices: List[str] = ["rock", "paper", "scissors"]
         self.player_name: str = name
 
-
-    def get_player_choice(self)-> str:
+    def get_player_choice(self) -> str:
         """Prompt the player to enter their choice of rock, paper, or scissors."""
-        player_choice = input(f'{self.player_name}, enter your choice ({self.choices}): ').lower()
+        player_choice = input(
+            f"{self.player_name}, enter your choice ({self.choices}): "
+        ).lower()
         if player_choice in self.choices:
             return player_choice
-        print(f'{self.player_name}, enter a right choices of {self.choices}')
+        print(f"{self.player_name}, enter a right choices of {self.choices}")
         return self.get_player_choice()
-
 
     def get_computer_choice(self) -> str:
         """Randomly select the computer's choice from choices of rock, paper, scissors."""
         return random.choice(self.choices)
-
 
     def decide_winner(self, user_choice: str, computer_choice: str) -> str:
         """Determine the winner of the game round based on user and computer choices.
@@ -49,12 +44,11 @@ class RockPaperScissors:
         :return: The result of the game as a string message who is the winner or if it's a tie.
         """
         if user_choice == computer_choice:
-            return 'It`s a tie!'
+            return "It`s a tie!"
         for win_comb in self.WIN_COMBINATIONS:
             if (user_choice == win_comb[0]) and (computer_choice == win_comb[1]):
-                return f'Congratulations {self.player_name}! You WIN'
-        return 'OH, sorrry, maybe you will win next time!...'
-
+                return f"Congratulations {self.player_name}! You WIN"
+        return "OH, sorrry, maybe you will win next time!..."
 
     def play(self):
         """Play a round of Rock, Paper, Scissors.
@@ -64,20 +58,22 @@ class RockPaperScissors:
         """
         user_choice = self.get_player_choice()
         computer_choice = self.get_computer_choice()
-        print(f'Your choice was: {user_choice}\nComputer choice was: {computer_choice}')
+        print(f"Your choice was: {user_choice}\nComputer choice was: {computer_choice}")
         print(self.decide_winner(user_choice, computer_choice))
 
 
 if __name__ == "__main__":
-    print('Welcome to the Rock, Paper, Scissors game!')
-    user_name = input('What is your name? ')
+    print("Welcome to the Rock, Paper, Scissors game!")
+    user_name = input("What is your name? ")
     game = RockPaperScissors(user_name)
 
     while True:
         game.play()
 
-        continue_game = input('Do you want to play again?'
-        '(Enter any key to continue, otherwise press q/Q to exit)')
-        if continue_game.lower() == 'q':
-            print('Thank you for playing! Goodbye!')
+        continue_game = input(
+            "Do you want to play again?"
+            "(Enter any key to continue, otherwise press q/Q to exit)"
+        )
+        if continue_game.lower() == "q":
+            print("Thank you for playing! Goodbye!")
             break
